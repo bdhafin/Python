8ea5e236472b87543a4a4851a1f1669eb2cce1eb
dhafinr <dhafinriz050500@gmail.com>
2019-11-20 16:15:15 +0700

    add string other func

---
+++ #3Strings and Slice them up/main2.py
@@ -0,0 +1,19 @@
+# |p|i|s|a|n|g| |g|o|r| e| n| g|
+# | | | | | | | | | | |  |  |  |
+# 0 1 2 3 4 5 6 7 8 9 10 11 12 13
+
+text  = "pisang goreng"
+
+#Cara mengambil elemen dari string
+a = text[5]
+
+#Cara lain mengambil elemen (ke belakang)
+b = text[-2]
+
+#Mengambil karakter panjang tertentu
+c = text[0:5]
+
+#mengambil karakter panjang sampai akhir
+d = text[2:]
+
+print(d)
