074dacd6da5e3a90a0ac54f2e99d7dbebb30c61c
dhafinr <dhafinriz050500@gmail.com>
2019-11-20 15:54:15 +0700

    delete a and b from view

--- #2Numbers dan Operasi/main.py
+++ #2Numbers dan Operasi/main.py
@@ -6,24 +6,24 @@ print("Nilai A : ", a)
 print("Nilai B : ", b, "\n")
 
 #Penjumlahan
-print("Penjumlahan a(", a,") + b(", b,") = ", a + b)
+print("Penjumlahan a + b = ", a + b)
 
 #Pengurangan
-print("Pengurangan a(", a,") - b(", b,") = ", a - b)
+print("Pengurangan a - b = ", a - b)
 
 #Perkalian
-print("Perkalian a(", a,") * b(", b,") = ", a * b)
+print("Perkalian a * b = ", a * b)
 
 #Pembagian
-print("Pembagian a(", a,") / b(", b,") = ", a / b)
+print("Pembagian a / b = ", a / b)
 
 #Modulus
-print("Modulus a(", a,") % b(", b,") = ", a % b)
+print("Modulus a % b = ", a % b)
 
 #Sisa Bagi di bulatkan ke bawah
-print("Sisa Bagi Dibulatkan a(", a,") // b(", b,") = ", a // b)
+print("Sisa Bagi Dibulatkan a // b = ", a // b)
 
 #Pangkat
-print("Perpangkatan a(", a,") ** b(", b,") = ", a ** b)
+print("Perpangkatan a ** b = ", a ** b)
 
 
