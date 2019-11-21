699a15d54c7872ee38db3c46ae254d7593cd6750
dhafinr <dhafinriz050500@gmail.com>
2019-11-21 09:49:23 +0700

    part 1

---
+++ #5If Elif Else/main.py
@@ -0,0 +1,35 @@
+nilai = 75
+# nilai2 = 80
+
+#Contoh nesting if
+# if nilai1 == 75:
+#    print("nilai anda : ", nilai1)
+#    print("Step 1")
+#    if nilai2 == 80:
+#        print("nilai anda : ", nilai2)
+#        print("Step 2")
+
+# is sama kaya ==
+if nilai is 75:
+   print("nilai anda : ", nilai)
+
+# is not sama kaya !=
+if nilai is not 60:
+     print("nilai anda : ", nilai)
+
+print("==" * 50)
+#Contoh penerapan if dan elif
+
+nilaiUjian = 70
+
+if 80 <= nilaiUjian <= 100:
+    print("nilai anda adalah A")
+elif 70 <= nilaiUjian < 80:
+    print("Nilai anda adalah B")
+elif 60 <= nilaiUjian < 70:
+    print("Nilai anda adalah C")
+elif 50 <= nilaiUjian < 60:
+    print("Nilai anda adalah D, lalukan perbaikan")
+else:
+    print("maaf anda tidak lulus mata kuliah ini")
+        
