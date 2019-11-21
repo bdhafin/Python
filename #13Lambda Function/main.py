79bdbcbf3384d4dfd463cafd3b788aa4f7f676b4
dhafinr <dhafinriz050500@gmail.com>
2019-11-21 13:51:03 +0700

    complete

---
+++ #13Lambda Function/main.py
@@ -0,0 +1,13 @@
+# Cara membuat fungsi biasa
+def jumlah(a, b):
+    c = a + b
+    return c
+
+
+hasil = jumlah(4, 5)
+print(hasil)
+
+#Cara membuat fungsi lambda
+kali = lambda x, y: x * y
+hasil = kali(3, 4) # bisa tanpa return
+print(hasil)
