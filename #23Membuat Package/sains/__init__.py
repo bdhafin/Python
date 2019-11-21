6bde72459e0248493c00d807c66abb424e1c1abc
dhafinr <dhafinriz050500@gmail.com>
2019-11-22 01:24:54 +0700

    add init for module package

---
+++ #23Membuat Package/sains/__init__.py
@@ -0,0 +1,8 @@
+#Ini file yang akan di eksekusi ketika panggil sains
+
+
+#mengambil dari module lain
+#artinya . (titik) adalah di folder sekarang 
+from .matematika import *
+#mengambil module fisika
+from .fisika import *
