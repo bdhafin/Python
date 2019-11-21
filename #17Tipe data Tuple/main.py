c95d9a08184b54b5e11ec07cd297b8ecdf56e2ad
dhafinr <dhafinriz050500@gmail.com>
2019-11-21 22:16:00 +0700

    complete intro tuples

---
+++ #17Tipe data Tuple/main.py
@@ -0,0 +1,14 @@
+Ganjil = [1, 3, 5, 7, 9]
+#Bentuk dasar dari tuples
+Genap = (2, 4, 6, 8 ,10)
+
+#Cara mengetahui tipe dari sebuah variable
+print(type(Ganjil))
+print(type(Genap))
+
+#Cara mengetahui apa saja yang bisa dilakukan dengan suatu data
+print(dir(Genap))
+#Menhitung element tertentu
+print(Genap.count(6))
+#Mengetahui posisi dari sebuah element (index)
+print(Genap.index(6))
