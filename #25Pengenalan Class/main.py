80f813394bdf1b59d406cc85efb2091e839e051e
dhafinr <dhafinriz050500@gmail.com>
2019-11-22 01:45:57 +0700

    complete class

---
+++ #25Pengenalan Class/main.py
@@ -0,0 +1,17 @@
+#Cara membuat class
+class Mahasiswa():
+    nama = 'nama'
+
+#instances
+dhafin = Mahasiswa()
+rizqullah = Mahasiswa()
+
+print(dhafin)
+print(rizqullah)
+#merubah atribut
+dhafin.nama = 'dhafin rizqullah'
+rizqullah.nama = 'muhammad rizqullah'
+
+#cara mengambil atribut
+print(dhafin.nama)
+print(rizqullah.nama)
