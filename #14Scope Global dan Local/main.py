0298493ae3dde94bb2aacbd97e5ba0afcfa663f8
dhafinr <dhafinriz050500@gmail.com>
2019-11-21 15:24:53 +0700

    complete

---
+++ #14Scope Global dan Local/main.py
@@ -0,0 +1,19 @@
+# scope variable 
+
+namaKucing = 'pretty'
+makananKucing = 'royal canin'
+
+def rubah_nama_kucing(namaBaru):
+    # ini variable local
+    namaKucing = namaBaru
+    print('Saya rubah nama kucing menjadi', namaKucing)
+
+def kasih_makan_kucing(makanan, nama):
+    #mengakses variable global
+    global namaKucing,makananKucing
+    namaKucing = nama
+    makananKucing = makanan
+
+rubah_nama_kucing('mimi')
+kasih_makan_kucing('universal', 'Jimmy')
+print('Nama kucing saya menjadi', namaKucing,'dan makan', makananKucing)
