37273efb28ccd10d7bff8a0abd878481f8c68d5c
dhafinr <dhafinriz050500@gmail.com>
2019-11-21 12:12:22 +0700

    complete

---
+++ #8Continue dan Pass/main.py
@@ -0,0 +1,11 @@
+for i in range(1, 10):
+    if i is 6:
+        print('nilai i adalah', 6)
+        #continue : untuk menskip proses di bawah nya dan kembali looping ke selanjutnya
+        pass #untuk hanya sebagai penanda (kata kunci kosong) dan tidak berpengaruh apa apa
+    print("nilai saat ini adalah", i)
+
+else:
+    print('akhir dari loop')
+
+print('print di luar loop')
