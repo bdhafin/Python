b71d434784f1eff6bdfadece0a841715c6d30361
dhafinr <dhafinriz050500@gmail.com>
2019-11-22 01:34:41 +0700

    complete matematika module

--- #24Menggunakan __main__/matematika.py
+++ #24Menggunakan __main__/matematika.py
@@ -3,3 +3,12 @@ def tambah(a, b):
 
 def kurang(a, b):
     print(a, ' -', b, '=', a - b)
+
+print(__name__) #hasil nya __main__
+
+def main():
+    print('ini adalah fungsi utama dari matematika')
+
+#mengecek bahwa ini di jalankan di file utama dan bukan di file lain
+if __name__ == '__main__':
+    main()
